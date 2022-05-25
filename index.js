5bb043435f262ade5bc73c8f635b9dce583aac75
mdkawsarali <facebookerjordan@gmail.com>
2022-05-26 04:27:42 +0600

    User: Get/add/update/delete

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
 // Mongodb configuration
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jnwe0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 // Verify JWT
 const verifyJWT = (req, res, next) => {
@@ -44,6 +44,17 @@ async function run() {
         const productCollection = client.db("AutimaPro").collection("products");
         const userCollection = client.db("AutimaPro").collection("user");
 
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' });
+            }
+        }
+
         // Authentication
         app.put('/token/:email', async (req, res) => {
             const email = req.params.email;
@@ -69,11 +80,30 @@ async function run() {
         });
 
         // Get User
-        app.get('/user', async (req, res) => {
+        app.get('/user', verifyJWT, verifyAdmin, async (req, res) => {
             const cursor = userCollection.find().sort({ '_id': -1 });
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        // Make Admin
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        // Delete User
+        app.delete('/user/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await userCollection.deleteOne(filter);
+            res.send(result);
+        })
 
         // Get Admin or not
         app.get('/admin/:email', async (req, res) => {
